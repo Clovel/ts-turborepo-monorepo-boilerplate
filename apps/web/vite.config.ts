6f37b47fad03ca310efb2859d8df80f2b62729a2
Clovis Durand <cd.clovel19@gmail.com>
2023-02-03 11:55:03 +0100

    Updated @/web's Vite configuration
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 const ViteConfig = defineConfig(
   {
     plugins: [
       react(),
     ],
+    resolve: {
+      alias: {
+        '*': path.resolve(__dirname, './src/'),
+        App: `${path.resolve(__dirname, './src/App/')}`,
+        assets: `${path.resolve(__dirname, './src/assets/')}`,
+        components: `${path.resolve(__dirname, './src/components/')}`,
+        helpers: `${path.resolve(__dirname, './src/helpers/')}`,
+        hooks: `${path.resolve(__dirname, './src/hooks/')}`,
+        layouts: `${path.resolve(__dirname, './src/layouts/')}`,
+        routes: `${path.resolve(__dirname, './src/routes/')}`,
+        pages: path.resolve(__dirname, './src/pages'),
+        types: `${path.resolve(__dirname, './src/@types')}`,
+        public: `${path.resolve(__dirname, './public/')}`,
+      },
+    },
   }
 );
 
